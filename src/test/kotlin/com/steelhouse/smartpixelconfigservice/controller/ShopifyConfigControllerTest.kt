0ff66b7b26d9bd7e3852c777723cb64edcef70ed
angelazhou-mntn <angela@mountain.com>
2024-03-11 18:21:02 -0700

    PX-719: refactor code to return 400 for aid with no update is needed

--- src/test/kotlin/com/steelhouse/smartpixelconfigservice/controller/ShopifyConfigControllerTest.kt
+++ src/test/kotlin/com/steelhouse/smartpixelconfigservice/controller/ShopifyConfigControllerTest.kt
@@ -1,10 +1,12 @@
 package com.steelhouse.smartpixelconfigservice.controller
 
 import com.steelhouse.smartpixelconfigservice.service.ShopifyConfigService
+import com.steelhouse.smartpixelconfigservice.service.Status
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 
 class ShopifyConfigControllerTest {
@@ -14,9 +16,28 @@ class ShopifyConfigControllerTest {
     @Test
     fun `migrateConversionPixel returns exception message for db error`() {
         val exceptionMsg = "dummy exception message"
-        every { service.migrateConversionPixel(1) }.throws(Exception(exceptionMsg))
+        every { service.migrateConversionPixel(1) } answers { Status(false, exceptionMsg) }
         val response = controller.migrateConversionPixel(1)
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.statusCode)
         assertEquals(exceptionMsg, response.body)
         assertEquals(MediaType.TEXT_PLAIN_VALUE, response.headers.contentType.toString())
+    }
+
+    @Test
+    fun `migrateConversionPixel returns status message from db transaction`() {
+        val statusMsg = "dummy local message"
+        every { service.migrateConversionPixel(1) } answers { Status(true, statusMsg) }
+        val response = controller.migrateConversionPixel(1)
+        assertEquals(HttpStatus.BAD_REQUEST, response.statusCode)
+        assertEquals(statusMsg, response.body)
+        assertEquals(MediaType.TEXT_PLAIN_VALUE, response.headers.contentType.toString())
+    }
+
+    @Test
+    fun `migrateConversionPixel happy path`() {
+        every { service.migrateConversionPixel(1) } answers { Status(true, null) }
+        val response = controller.migrateConversionPixel(1)
+        assertEquals(HttpStatus.OK, response.statusCode)
+        assertEquals(null, response.body)
     }
 }
